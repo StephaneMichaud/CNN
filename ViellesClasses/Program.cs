11988ecab87e97a4be7d56619570542c3589c65e
Ludovic Bédard <ludovic.bedard@gmail.com>
2019-01-05 13:32:14 -0500

    ajout des vielles classes.

---
+++ ViellesClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            
+            PN pooling = new PN(2, 2, 28, 28, 3);
+            CN convolutionnal = new CN(2, 2, 28, 28, 3, 1, 4);
+            FCN reLuLayer = new FCN(new int[] { },FCN.ReLU,FCN.ReLUPrime,generator);
+            SubNetwork[] architecture = new SubNetwork[] { convolutionnal,pooling,convolutionnal,pooling,convolutionnal,pooling,reLuLayer};
+            CNN network = new CNN(architecture, generator);
+
+            network.FeedForward(new Matrix[0]);
+        }
+    }
+}
